8b3ebe7ba3c6ad962648889312c2d7de6d93ccb5
Ahmet Ozan Ekici <aozan.ekici@gmail.com>
2017-01-15 14:06:41 +0300

    düzenleme

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-http.listen(PORT, function () {
-  console.log('Server Started');
-});
 
-app.get('/index',function(req, res) {
-  // var balance = getBalance();
+
+app.get('/balance',function(req, res) {
   var url = 'https://servisler.izmir.bel.tr/Service1.svc?wsdl';
   var args = {kartNumarasi: '23107154676'};
   soap.createClient(url, function(err, client) {
     if (typeof client !== 'undefined') {
        client.UlasimKartiBakiyesiGetir(args, function(err, result) {
-        //  $('#balance').text(result.UlasimKartiBakiyesiGetirResult);
-          //  console.log(result.UlasimKartiBakiyesiGetirResult);
           res.send(result.UlasimKartiBakiyesiGetirResult);
        });
     }
   });
-  // res.send(balance);
 });
 
-function getBalance() {
 
-}
+http.listen(PORT, function () {
+  console.log('Server Started on ' +PORT);
+});
